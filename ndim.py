642e70dcd927bc4d24188ace6114f58259886cf8
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-06-21 10:14:17 +0530

    Add files via upload
    Function of Array

---
+++ ndim.py
@@ -0,0 +1,6 @@
+#BELOW IS THE PROGRAMME OF WHEN I HAVE TO CHECK OUR DIMENSIONS OF OUR ARAAY
+#this is 3D array prog and i have to check 
+'''import numpy as np
+a1=[[[3,4,5,6,7],[4,3,2,1,0]]]
+arr1=np.array(a1)
+print(arr1.ndim)'''
