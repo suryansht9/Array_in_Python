41a46901e624ae6aeee93dc3a3006a6bfcd71b05
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-06-22 09:59:19 +0530

    these all are functions of array

---
+++ ndmin.py
@@ -0,0 +1,7 @@
+#BELOW IS THE PROGRAMME OF WHEN I HAVE TO GIVE DIMENSIONS ACCORDING TO ME
+import numpy as np
+
+a1 = [[4, 5, 7], [7, 8, 9]]
+arr1 = np.array(a1, ndmin=10)
+print(arr1)
+print(arr1.shape)
