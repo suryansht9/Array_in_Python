bd8432b6fcb1a3f9e0dbc411394917380bd71d0f
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-06-24 20:07:13 +0530

    Update shape.py

--- shape.py
+++ shape.py
@@ -6,3 +6,12 @@ a1 = [[4, 5, 7], [7, 8, 9]]
 arr1 = np.array(a1)
 print(arr1)
 print(arr1.shape)
+
+
+
+#Create an array with 5 dimensions using ndmin using a vector with values 1,2,3,4 and verify that last dimension has value 4:
+
+import numpy as np
+myarr=([1,2,3,4],ndmin=5)
+print(myarr)
+print(myarr.shape,"This is the 5d dimension after applying ndmin according to ques")
